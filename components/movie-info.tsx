852930043da1b9c19ca98980c2514a3fe090a185
minnje <167839981+minnje@users.noreply.github.com>
2025-08-11 22:09:13 +0900

    movie page & generateMetadata

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,11 +1,29 @@
+import style from "../styles/movie-info.module.css";
 import { API_URL } from "../app/(home)/page";
 
-async function getMovie(id: string) {
-     const response = await fetch(`${API_URL}}/${id}`);
+export async function getMovie(id: string) {
+     const response = await fetch(`${API_URL}/${id}`);
      return response.json();
 }
 
 export default async function MovieInfo({ id }: { id: string }) {
      const movie = await getMovie(id);
-     return <h6>{JSON.stringify(movie)}</h6>;
+
+     return (
+          <div className={style.container}>
+               <img
+                    src={movie.poster_path}
+                    className={style.poster}
+                    alt={movie.title}
+               />
+               <div className={style.info}>
+                    <h1 className={style.title}>{movie.title}</h1>
+                    <h3>⭐️ {movie.vote_average.toFixed(1)}</h3>
+                    <p>{movie.overview}</p>
+                    <a href={movie.homepage} target={"_blank"}>
+                         Homepage &rarr;
+                    </a>
+               </div>
+          </div>
+     );
 }
